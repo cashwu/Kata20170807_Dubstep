87d2ecbb76ac97d462b25d8635613d9069f688be
cash <cash@cashwu.com>
2017-08-07 16:42:10 +0800

    input AWUB test case

--- Kata20170807_Dubstep/DubstepTests.cs
+++ Kata20170807_Dubstep/DubstepTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170807_Dubstep
     public class DubstepTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_AWUB_should_return_A()
         {
+            DubstepSongDecoderShouldBe("A", "AWUB");
+        }
+
+        private static void DubstepSongDecoderShouldBe(string expected, string input)
+        {
+            var dubstep = new Dubstep();
+            var actual = dubstep.SongDecoder(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Dubstep
+    {
+        public string SongDecoder(string input)
+        {
+            return "A";
         }
     }
 }

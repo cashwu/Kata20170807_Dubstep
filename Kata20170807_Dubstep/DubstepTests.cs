f5092bb847325e66d10a2db5785c6ebe5812d458
cash <cash@cashwu.com>
2017-08-07 16:22:20 +0800

    init

--- Kata20170807_Dubstep/DubstepTests.cs
+++ Kata20170807_Dubstep/DubstepTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170807_Dubstep
 {
     [TestClass]
-    public class UnitTest1
+    public class DubstepTests
     {
         [TestMethod]
         public void TestMethod1()
